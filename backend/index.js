aa3baaf6c386504c86431db7187a8652b3453d32
parikhasharma <sharmaparikha@gmail.com>
2024-03-09 09:13:32 +0530

    ME_BUILDOUT_XFLIX_NODE_MODULE_NODE  (Crio.Do)

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.NODE_ENV;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // ------------------------MongoDB Server----------------------------------
